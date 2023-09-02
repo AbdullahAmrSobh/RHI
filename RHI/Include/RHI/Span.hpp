b6e926bef1105ac9adb031b46bba58a6a23f2283
AbdullahAmrSobh <abdullahamrsobh@gmail.com>
2023-09-02 13:41:27 +0300

    Update span implementation

--- RHI/Include/RHI/Span.hpp
+++ RHI/Include/RHI/Span.hpp
@@ -18,49 +18,57 @@ namespace TL
 template<typename ElementType>
 class Span
 {
-    static_assert(std::is_object<ElementType>::value,
-                  "A span's ElementType must be an object type (not a "
-                  "reference type or void)");
-    static_assert(!std::is_abstract<ElementType>::value,
-                  "A span's ElementType cannot be an abstract class type");
+    static_assert(std::is_object_v<ElementType>, "ElementType must be an object type");
+    static_assert(!std::is_abstract_v<ElementType>, "ElementType cannot be an abstract class type");
 
 public:
-    // constants and types
-    using element_type     = ElementType;
-    using value_type       = typename std::remove_cv<ElementType>::type;
-    using size_type        = std::size_t;
-    using difference_type  = std::ptrdiff_t;
-    using pointer          = element_type*;
-    using const_pointer    = const element_type*;
-    using reference        = element_type&;
-    using const_reference  = const element_type&;
-    using iterator         = pointer;
-    using reverse_iterator = std::reverse_iterator<iterator>;
+    using SizeType  = std::size_t;
+    using Pointer   = ElementType*;
+    using Reference = ElementType&;
+    using Iterator  = Pointer;
 
-    Span() = default;
+    constexpr Span(ElementType& type) noexcept
+        : m_data(&type)
+        , m_count(1)
+    {
+    }
 
-    constexpr Span(pointer element, size_type count) noexcept
-        : m_values(element)
+    constexpr Span(Pointer element, SizeType count) noexcept
+        : m_data(element)
         , m_count(count)
     {
     }
 
-    constexpr Span(pointer firstElement, pointer lastElement) noexcept
-        : m_values(firstElement)
+    constexpr Span(Pointer firstElement, Pointer lastElement) noexcept
+        : m_data(firstElement)
         , m_count(lastElement - firstElement)
     {
         assert(lastElement > firstElement);
     }
 
-    constexpr Span(std::initializer_list<ElementType> elements) noexcept
-        : m_values(elements.begin())
-        , m_count(elements.end() - elements.end())
+    constexpr Span(const std::initializer_list<ElementType>& elements) noexcept
+        requires std::is_const_v<ElementType>
+        : m_data(elements.begin())
+        , m_count(elements.size())
     {
     }
 
     constexpr Span(std::vector<ElementType>& elements) noexcept
-        : m_values(elements.data())
+        : m_data(elements.data())
         , m_count(elements.size())
+    {
+    }
+
+    constexpr Span(const std::vector<ElementType>& elements) noexcept
+        : m_data(elements.data())
+        , m_count(elements.size())
+    {
+    }
+
+    template<size_t N>
+    constexpr Span(ElementType (&array)[N])
+        : m_data(array)
+        , m_count(sizeof(array) / sizeof(array[0]))
     {
     }
 
@@ -69,9 +77,9 @@ public:
         return m_count;
     }
 
-    RHI_NODISCARD constexpr size_type size_bytes() const noexcept
+    RHI_NODISCARD constexpr SizeType size_bytes() const noexcept
     {
-        return m_count * sizeof(element_type);
+        return m_count * sizeof(ElementType);
     }
 
     RHI_NODISCARD constexpr bool empty() const noexcept
@@ -79,39 +87,39 @@ public:
         return m_count == 0;
     }
 
-    RHI_NODISCARD constexpr reference operator[](const size_type index) const noexcept
+    RHI_NODISCARD constexpr Reference operator[](const SizeType index) const noexcept
     {
-        return m_values[index];
+        return m_data[index];
     }
 
-    RHI_NODISCARD constexpr reference front() const noexcept
+    RHI_NODISCARD constexpr Reference front() const noexcept
     {
-        return m_values[0];
+        return m_data[0];
     }
 
-    RHI_NODISCARD constexpr reference back() const noexcept
+    RHI_NODISCARD constexpr Reference back() const noexcept
     {
-        return m_values[m_count - 1];
+        return m_data[m_count - 1];
     }
 
-    RHI_NODISCARD constexpr pointer data() const noexcept
+    RHI_NODISCARD constexpr Pointer data() const noexcept
     {
-        return m_values;
+        return m_data;
     }
 
-    RHI_NODISCARD constexpr iterator begin() const noexcept
+    RHI_NODISCARD constexpr Iterator begin() const noexcept
     {
-        return m_values;
+        return m_data;
     }
 
-    RHI_NODISCARD constexpr iterator end() const noexcept
+    RHI_NODISCARD constexpr Iterator end() const noexcept
     {
-        return m_values + m_count;
+        return m_data + m_count;
     }
 
 private:
-    const ElementType* m_values;
-    size_type    m_count;
+    Pointer  m_data;
+    SizeType m_count;
 };
 
 }  // namespace TL
