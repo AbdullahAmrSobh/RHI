82b18b4511ea10716862f9249f6e52ded84bcddb
AbdullahAmrSobh <abdullahamrsobh@gmail.com>
2023-09-05 22:46:17 +0300

    Update Handle implementation

--- RHI/Include/RHI/Handle.hpp
+++ RHI/Include/RHI/Handle.hpp
@@ -18,7 +18,7 @@ public:
         m_packedHandle = UINT64_MAX;
     }
 
-    Handle(uint32_t id, uint16_t genId)
+    Handle(uint64_t id, uint16_t genId)
     {
         m_data.index        = id;
         m_data.generationId = genId;
@@ -113,6 +113,22 @@ public:
         }
     }
 
+    // Gets the descriptor associated with handle.
+    RHI_FORCE_INLINE Descriptor& GetDescriptor(Handle<Descriptor> handle) const
+    {
+        uint32_t index        = handle.m_index;
+        uint16_t generationID = handle.m_generetionId;
+
+        if (!IsValid(Handle))
+        {
+            return {};
+        }
+        else
+        {
+            return m_descriptors[index];
+        }
+    }
+
     // Inserts a new resource and returns its handle.
     RHI_FORCE_INLINE Handle<Resource> Insert(Resource resource, Descriptor descriptor)
     {
@@ -177,6 +193,121 @@ private:
     // as this is a very in-effiecent implementation.
     std::vector<Resource>   m_resources;
     std::vector<Descriptor> m_descriptors;
+    std::vector<int32_t>    m_generetionID;
+
+    // Maintain a list of available slots (indices) for reuse
+    std::vector<uint32_t> m_availableSlots;
+};
+
+/// @brief Represents a storage where RHI objects live.
+template<typename Resource>
+class HandlePool<Resource, void>
+{
+public:
+    HandlePool(const HandlePool& handles) = delete;
+
+    RHI_FORCE_INLINE HandlePool(uint32_t capacity)
+    {
+        m_resources.resize(capacity);
+        m_generetionID.resize(capacity, 0);
+    }
+
+    // Remove all stored handles, and resets the graph
+    RHI_FORCE_INLINE void Reset()
+    {
+        m_resources.clear();
+        m_generetionID.clear();
+        m_availableSlots.clear();
+    }
+
+    RHI_FORCE_INLINE bool IsValid(Handle<Resource> handle) const
+    {
+        uint32_t index        = handle.m_index;
+        uint16_t generationID = handle.m_generetionId;
+        if (index < m_resources.size() && generationID == m_generetionID[index])
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // Gets the resource associated with handle.
+    RHI_FORCE_INLINE Resource GetResource(Handle<Resource> handle) const
+    {
+        uint32_t index        = handle.m_index;
+        uint16_t generationID = handle.m_generetionId;
+
+        if (!IsValid(Handle))
+        {
+            return {};
+        }
+        else
+        {
+            return m_resources[index];
+        }
+    }
+
+    // Inserts a new resource and returns its handle.
+    RHI_FORCE_INLINE Handle<Resource> Insert(Resource resource)
+    {
+        // Reuse a slot if available, otherwise allocate a new one
+        uint32_t index;
+        if (!m_availableSlots.empty())
+        {
+            index = m_availableSlots.back();
+            m_availableSlots.pop_back();
+        }
+        else
+        {
+            index = m_resources.size();
+            m_generetionID.push_back(1);
+        }
+
+        // Update the resources and descriptors vectors
+        if (index >= m_resources.size())
+        {
+            m_resources.push_back(resource);
+        }
+        else
+        {
+            m_resources[index]   = resource;
+        }
+
+        return Handle(index, m_generetionID[index]);
+    }
+
+    // Gets all objects that are currently alive.
+    RHI_FORCE_INLINE std::vector<Handle<Resource>> GetLiveHandles() const
+    {
+        std::vector<Handle<Resource>> liveHandles;
+        for (uint32_t i = 0; i < m_resources.size(); i++)
+        {
+            if (m_resources[i])
+            {
+                liveHandles.push_back(Handle(i, m_generetionID[i]));
+            }
+        }
+        return liveHandles;
+    }
+
+    // Removes a resource from the owner.
+    RHI_FORCE_INLINE void Remove(Handle<Resource> handle)
+    {
+        uint32_t index        = handle.m_index;
+        uint16_t generationID = handle.m_generetionId;
+
+        if (index < m_resources.size() && generationID == m_generetionID[index])
+        {
+            m_resources[index] = Handle<Resource>();
+            m_generetionID[index]++;
+            m_availableSlots.push_back(index);  // Add the slot to available slots
+        }
+    }
+
+private:
+    // TODO rewrite it such that it does not depend on std::vector.
+    // as this is a very in-effiecent implementation.
+    std::vector<Resource>   m_resources;
     std::vector<int32_t>    m_generetionID;
 
     // Maintain a list of available slots (indices) for reuse
