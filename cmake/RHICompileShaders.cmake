1dd3baa8281476d09fe2589868943e4909bedb29
AbdullahAmrSobh <abdullahamrsobh@gmail.com>
2023-10-13 18:18:55 +0300

    Add CMake custom target for compiling shaders

---
+++ cmake/RHICompileShaders.cmake
@@ -0,0 +1,64 @@
+include(CMakeParseArguments)
+
+# Compiles the resources to OUTPUT_DIR and creates a new target "{TARGET}-compile-shaders"
+function(RHICompileShaders)
+	cmake_parse_arguments(
+		RHI_ARG # prefix of output variables
+		"" # list of names of boolean flags (defined ones will be true)
+		"TARGET;NAME;OUTPUT_DIR" # single valued arguments
+		"DEPENDENCIES;SHADER_FILES;BLOB_FILES" # list values arguments
+		${ARGN} # the argument that will be parsed
+	)
+
+	# Generate shader files after compilation
+	set(RHI_OUTPUT_SHADER_FILES)
+	foreach(RHI_SHADER_ABS_PATH ${RHI_ARG_SHADER_FILES})
+		get_filename_component(RHI_SHADER_NAME ${RHI_SHADER_ABS_PATH} NAME_WE)
+		get_filename_component(RHI_SHADER_DIR ${RHI_SHADER_ABS_PATH} DIRECTORY)
+
+		# make sure the compiled shaders folders exists before actually compiling them
+		file(MAKE_DIRECTORY ${RHI_ARG_OUTPUT_DIR})
+
+		set(RHI_OUTPUT_VERT_SPV_SHADER_NAME "${RHI_ARG_OUTPUT_DIR}/${RHI_SHADER_NAME}.vertex.spv")
+		set(RHI_OUTPUT_PIXEL_SPV_SHADER_NAME "${RHI_ARG_OUTPUT_DIR}/${RHI_SHADER_NAME}.pixel.spv")
+		set(RHI_OUTPUT_SPV_SHADER_MODULE_NAME "${RHI_ARG_OUTPUT_DIR}/${RHI_SHADER_NAME}.spv")
+
+        # compile vertex stage
+		add_custom_command(
+			OUTPUT ${RHI_OUTPUT_VERT_SPV_SHADER_NAME}
+			COMMAND dxc -spirv ${RHI_SHADER_ABS_PATH} -fspv-reflect -fspv-target-env=vulkan1.3 -E VSMain -T vs_6_7 -Fo ${RHI_OUTPUT_VERT_SPV_SHADER_NAME}
+			DEPENDS ${RHI_SHADER_ABS_PATH} ${RHI_ARG_DEPENDENCIES}
+			COMMENT "Compiling ${RHI_SHADER_NAME} vertex stage"
+		)
+
+        # compile pixel stage
+		add_custom_command(
+			OUTPUT ${RHI_OUTPUT_PIXEL_SPV_SHADER_NAME}
+			COMMAND dxc -spirv ${RHI_SHADER_ABS_PATH} -fspv-reflect -fspv-target-env=vulkan1.3 -E PSMain -T ps_6_7 -Fo ${RHI_OUTPUT_PIXEL_SPV_SHADER_NAME}
+			DEPENDS ${RHI_SHADER_ABS_PATH} ${RHI_ARG_DEPENDENCIES}
+			COMMENT "Compiling ${RHI_SHADER_NAME} pixel stage"
+		)
+
+        # generate library
+		add_custom_command(
+			OUTPUT ${RHI_OUTPUT_SPV_SHADER_MODULE_NAME}
+			COMMAND spirv-link -o ${RHI_OUTPUT_SPV_SHADER_MODULE_NAME} --create-library ${RHI_OUTPUT_VERT_SPV_SHADER_NAME} ${RHI_OUTPUT_PIXEL_SPV_SHADER_NAME}
+			DEPENDS ${RHI_OUTPUT_VERT_SPV_SHADER_NAME} ${RHI_OUTPUT_PIXEL_SPV_SHADER_NAME}
+			COMMENT "Generating Library  ${RHI_SHADER_ABS_PATH}"
+		)
+
+		list(APPEND RHI_OUTPUT_SHADER_FILES "${RHI_OUTPUT_SPV_SHADER_MODULE_NAME}")
+	endforeach(RHI_SHADER_ABS_PATH)
+
+	# If we don't have any shaders we don't need to create a custom target for them
+	if (RHI_OUTPUT_SHADER_FILES)
+		add_custom_target(${RHI_ARG_TARGET}-shaders-${RHI_ARG_NAME} DEPENDS ${RHI_OUTPUT_SHADER_FILES})
+	endif()
+
+	# Make a new compile-resources target and add it to ${RHI_ARG_TARGET} dependencies
+	add_custom_target(${RHI_ARG_TARGET}-compile-shaders)
+
+	if (RHI_OUTPUT_SHADER_FILES)
+		add_dependencies(${RHI_ARG_TARGET}-compile-shaders ${RHI_ARG_TARGET}-shaders-${RHI_ARG_NAME})
+	endif()
+endfunction(RHICompileShaders)
