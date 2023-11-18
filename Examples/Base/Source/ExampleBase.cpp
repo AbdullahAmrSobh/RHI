31615f4e39ad0cd3177dd8d353be829455bc4e95
AbdullahAmrSobh <abdullahamrsobh@gmail.com>
2023-11-18 20:29:50 +0200

    Add stb image

--- Examples/Base/Source/ExampleBase.cpp
+++ Examples/Base/Source/ExampleBase.cpp
@@ -1,18 +1,20 @@
+#include "Examples-Base/ExampleBase.hpp"
+
+#include <RHI-Vulkan/Loader.hpp>
 #include <cassert>
 #include <fstream>
 #include <iostream>
 #include <memory>
 #include <string>
 
-#include "Examples-Base/ExampleBase.hpp"
-
-#include <RHI-Vulkan/Loader.hpp>
 
 #define GLFW_EXPOSE_NATIVE_WIN32
 #include <GLFW/glfw3.h>
 #include <GLFW/glfw3native.h>
 
 #undef LoadImage
+
+#include "stb_image.h"
 
 class DebugCallbacks final : public RHI::DebugCallbacks
 {
@@ -35,7 +37,32 @@ public:
 
 ImageData ExampleBase::LoadImage(std::string_view path) const
 {
-    return {};
+    // Load the image using stb_image
+    int width, height, channels;
+    stbi_set_flip_vertically_on_load(0); // Set to 0 to keep image orientation as is
+    unsigned char* data = stbi_load(path.data(), &width, &height, &channels, 0);
+
+    // Check if the image was loaded successfully
+    if (!data)
+    {
+        RHI_UNREACHABLE(); // ("Failed to load image: " + std::string(path));
+    }
+
+    // Create ImageData object
+    ImageData imageData;
+    imageData.width           = width;
+    imageData.height          = height;
+    imageData.depth           = 1; // Assuming single-layer images
+    imageData.channels        = channels;
+    imageData.bytesPerChannel = 1; // Assuming 8-bit per channel data
+
+    // Convert image data to std::vector<uint8_t>
+    imageData.data.assign(data, data + width * height * channels);
+
+    // Free the memory allocated by stb_image
+    stbi_image_free(data);
+
+    return imageData;
 }
 
 std::vector<uint32_t> ExampleBase::ReadBinaryFile(std::string_view path) const
@@ -72,27 +99,27 @@ ExampleBase::ExampleBase(std::string name, uint32_t width, uint32_t height)
     m_window = window;
 
     RHI::ApplicationInfo appInfo{};
-    appInfo.applicationName = "RHI-App";
+    appInfo.applicationName    = "RHI-App";
     appInfo.applicationVersion = RHI::MakeVersion(0, 1, 0);
-    auto debugCallbacks = std::make_unique<DebugCallbacks>();
-    m_context = RHI::CreateVulkanRHI(appInfo, std::move(debugCallbacks));
+    auto debugCallbacks        = std::make_unique<DebugCallbacks>();
+    m_context                  = RHI::CreateVulkanRHI(appInfo, std::move(debugCallbacks));
 }
 
 void ExampleBase::Init()
 {
     WindowInfo windowInfo{};
-    windowInfo.hwnd = glfwGetWin32Window(static_cast<GLFWwindow*>(m_window));
+    windowInfo.hwnd      = glfwGetWin32Window(static_cast<GLFWwindow*>(m_window));
     windowInfo.hinstance = NULL;
 
     // create swapchain
     RHI::SwapchainCreateInfo createInfo{};
-    createInfo.win32Window.hwnd = windowInfo.hwnd;
+    createInfo.win32Window.hwnd      = windowInfo.hwnd;
     createInfo.win32Window.hinstance = windowInfo.hinstance;
-    createInfo.imageSize.width = windowInfo.width;
-    createInfo.imageSize.height = windowInfo.height;
-    createInfo.imageUsage = RHI::ImageUsage::Color;
-    createInfo.imageFormat = RHI::Format::BGRA8_UNORM;
-    createInfo.imageCount = 3;
+    createInfo.imageSize.width       = windowInfo.width;
+    createInfo.imageSize.height      = windowInfo.height;
+    createInfo.imageUsage            = RHI::ImageUsage::Color;
+    createInfo.imageFormat           = RHI::Format::BGRA8_UNORM;
+    createInfo.imageCount            = 3;
 
     m_swapchain = m_context->CreateSwapchain(createInfo);
 
